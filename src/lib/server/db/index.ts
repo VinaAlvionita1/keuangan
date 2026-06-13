d018de52054f21e8017a3b0fa80d65e0de03b757
NazirArifin <ceylon.rizan@gmail.com>
2026-06-13 09:42:27 +0700

    feat: add dotenv dependency and update database connection handling

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,13 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
+import 'dotenv/config';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = mysql.createPool(env.DATABASE_URL);
+const poolConnection = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  connectionLimit: 10
+});
 
-export const db = drizzle(client, { schema, mode: 'default' });
+export const db = drizzle(poolConnection, { schema, mode: 'default' });
