89082d4b36da3775659a05b7315a291d5cceb6a9
NazirArifin <ceylon.rizan@gmail.com>
2026-06-24 11:14:13 +0700

    feat: update ecosystem configuration with additional environment variables

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
       script: "./build/index.js",
       env: {
         NODE_ENV: 'production',
-        PORT: 3002
+        PORT: 3002,
+        BODY_SIZE_LIMIT: '10M',
+        ORIGIN: 'https://administrasi.labinf.unira.ac.id',
+        PROTOCOL_HEADER: 'x-forwarded-proto',
+        HOST_HEADER: 'x-forwarded-host',
+        PORT_HEADER: 'x-forwarded-port'
       }
     }
   ]
